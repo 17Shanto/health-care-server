eabc4e69fde9e4e10f5895e80f7991421759e558
17Shanto <shahporanshanto23@gmail.com>
2026-01-30 17:31:35 +0600

    feat: validate Request middleware, create user rote and setup multer & cloudinary

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { UserService } from "./user.service";
+import sendResponse from "../../shared/sendResponse";
+import HttpStatus from "http-status";
+
+const createPatient = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.createPatient(req.body);
+  sendResponse(res, {
+    statusCode: HttpStatus.CREATED,
+    success: true,
+    message: "patient created successfully",
+    data: result,
+  });
+});
+
+export const UserController = {
+  createPatient,
+};
---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,26 @@
+import { prisma } from "../../../lib/prisma";
+import { createPatientInput } from "./user.interface";
+import bcrypt from "bcryptjs";
+
+const createPatient = async (payload: createPatientInput) => {
+  const hashPassword = await bcrypt.hash(payload.password, 10);
+  const result = await prisma.$transaction(async (tx) => {
+    await tx.user.create({
+      data: {
+        email: payload.email,
+        password: hashPassword,
+      },
+    });
+    return await tx.patient.create({
+      data: {
+        name: payload.name,
+        email: payload.email,
+      },
+    });
+  });
+  return result;
+};
+
+export const UserService = {
+  createPatient,
+};
