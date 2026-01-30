294ad5a754448303f7a9b04e708b3a34f7adc63e
17Shanto <shahporanshanto23@gmail.com>
2026-01-31 00:48:24 +0600

    feat: add token on cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -6,12 +6,27 @@ import { AuthService } from "./auth.service";
 
 const login = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.login(req.body);
+  const { accessToken, refreshToken, needPassWordChange } = result;
+
+  res.cookie("accessToken", accessToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+    maxAge: 1000 * 60 * 60,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+    maxAge: 1000 * 60 * 60 * 24 * 90,
+  });
   // console.log(req.body);
   sendResponse(res, {
     statusCode: HttpStatus.CREATED,
     success: true,
     message: "successfully login ✅",
-    data: result,
+    data: needPassWordChange,
   });
 });
 
